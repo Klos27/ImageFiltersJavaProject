0acdcf8089c4b064ccdfaba2c8e565fc48fc1480
Paweł Wądolny <viperowski@gmail.com>
2018-01-23 23:00:54 +0100

    Added load server info statistics

--- src/ProcessingServer/controller/ProcessingServerController.java
+++ src/ProcessingServer/controller/ProcessingServerController.java
@@ -1,16 +1,14 @@
 package ProcessingServer.controller;
 
-//javaFX
 import ProcessingServer.model.ProcessingServer;
 import ProcessingServer.model.ProcessingServerPriority;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-//javaNet
+import javafx.scene.control.*;
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+
 
 public class ProcessingServerController {
     static boolean ifServerIsRunning = false;
@@ -36,6 +34,32 @@ public class ProcessingServerController {
     private TextField serverPortField;
     @FXML
     private TextField loadInfoPortField;
+    //-------LOAD INFO
+    static LoadUpdater loadUpdaterHolder = null;
+    static OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(
+            OperatingSystemMXBean.class);
+    @FXML
+    private Label serverLoadLabel;
+    @FXML
+    private Label ramFreeLabel;
+    @FXML
+    private Label cpuSystemLabel;
+    @FXML
+    private Label cpuServerLabel;
+    @FXML
+    private Label ramUsageLabel;
+    @FXML
+    private Label ramTotalLabel;
+    @FXML
+    private Label ramForAppLabel;
+    @FXML
+    private ProgressBar cpuSystemBar;
+    @FXML
+    private ProgressBar cpuServerBar;
+    @FXML
+    private ProgressBar ramUsageBar;
+
+    //-------END OF LOAD INFO
     //Set information to Text Area
     @FXML
     private void setInfoToResultArea(String infoText) {
@@ -75,6 +99,8 @@ public class ProcessingServerController {
                 loadInfoPortField.setVisible(false);
                 infoLabel.setVisible(false);
                 resultArea.setText("Server is running");
+                loadUpdaterHolder = new LoadUpdater();
+                loadUpdaterHolder.start();
             } else {
                 appendInfoToResultArea("ProcessingServer is already running");
             }
@@ -105,12 +131,47 @@ public class ProcessingServerController {
             serverPriority.close();
             serverPriority.interrupt();
         }
+        if(loadUpdaterHolder != null && !loadUpdaterHolder.isInterrupted()){
+            loadUpdaterHolder.interrupt();
+        }
     }
     public static int getServerPort(){
         return serverPort;
     }
     public static int getLoadInfoPort(){
         return loadInfoPort;
+    }
+
+    public void updateLoad(){
+        // update all load info
+        serverLoadLabel.setText(String.valueOf(server.getLoad() / 1048576) + " MB");
+        ramFreeLabel.setText(String.valueOf(osBean.getFreePhysicalMemorySize() / 1048576) + " MB");
+        ramForAppLabel.setText(String.valueOf(osBean.getCommittedVirtualMemorySize() / 1048576) + " MB");
+        cpuSystemBar.setProgress(osBean.getSystemCpuLoad());
+        cpuServerBar.setProgress(osBean.getProcessCpuLoad());
+        ramUsageBar.setProgress((osBean.getTotalPhysicalMemorySize()- osBean.getFreePhysicalMemorySize())/(double)osBean.getTotalPhysicalMemorySize());
+        cpuSystemLabel.setText(String.format("%.2f", osBean.getSystemCpuLoad() * 100) + "%");
+        cpuServerLabel.setText(String.format("%.2f", osBean.getProcessCpuLoad() * 100) + "%");
+        ramUsageLabel.setText(String.format("%.2f", ((osBean.getTotalPhysicalMemorySize()- osBean.getFreePhysicalMemorySize())/(double)osBean.getTotalPhysicalMemorySize()) * 100) + "%");
+        ramTotalLabel.setText(String.valueOf(osBean.getTotalPhysicalMemorySize() / 1048576) + " MB");
+    }
+
+    class LoadUpdater extends Thread{
+        public void run(){
+            while(!Thread.currentThread().isInterrupted()){
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateLoad();
+                    }
+                });
+                try{
+                    Thread.currentThread().sleep(1000);
+                } catch (InterruptedException e) {
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
 
